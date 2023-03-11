ce8471701a52d0c0750f7b8f3e7117d09652eb6a
Chris Osborne <chrisosborne@Chriss-Air.broadband>
2023-03-11 17:30:52 +0000

    added foliumexample to urlpatterns

--- application/djangoserver/urls.py
+++ application/djangoserver/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('earthquaketracker/', include('earthquaketracker.urls')),
-    path('isstracker/', include('isstracker.urls'))
+    path('isstracker/', include('isstracker.urls')),
+    path('foliumexample/', include('foliumexample.urls'))
 ]
